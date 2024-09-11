993c457d5c3b7d5683f54885884c4daa5e6edf30
lornd <lornd@qq.com>
2024-09-11 11:19:23 +0800

    feat:修改模板配置文件路径以与代码中读取逻辑匹配

--- agent_network/main.py
+++ agent_network/main.py
@@ -2,7 +2,7 @@ from pipeline.pipeline import Pipeline
 from utils.logger import Logger
 
 if __name__ == '__main__':
-    config_dir = ""
+    config_dir = "agent_network/config"
     logger = Logger("log")
     pipeline = Pipeline(config_dir, logger)
     pipeline.agent()
