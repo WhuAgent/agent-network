ea6c62addee8c967d11049708cb126725541531a
lornd <lornd@qq.com>
2024-12-11 17:22:06 +0800

    feat:将 TBotAgent 的 logger 集成到 agent-network 中，优化日志记录

--- agent_network/utils/logger.py
+++ agent_network/utils/logger.py
@@ -15,13 +15,21 @@ class Logger:
         self.file_name = f"{prefix}-{self.start_time}.txt" if prefix else f"{self.start_time}.txt"
         self.file_path = os.path.join(self.log_dir, self.file_name)
 
-    def log(self, content):
+    def log(self, cur_time, role="", content="", class_name="", output=True):
         if not isinstance(content, str):
             content = json.dumps(content, indent=4, ensure_ascii=False)
+        buffer = "---------------------------------------------------\n"
+        buffer += f"[{cur_time}]"
+        if class_name:
+            buffer += f"[{class_name}]"
+        if role:
+            buffer += f"[{role}]"
+        buffer += f"\n{content}\n\n"
+
+        if output:
+            print(buffer)
         with open(self.file_path, 'a', encoding="UTF-8") as f:
-            f.write(content)
-            f.write("\n")
-            f.write("\n")
+            f.write(buffer)
 
     def rename(self, success):
         file_name, extension = self.file_name.split(".")
