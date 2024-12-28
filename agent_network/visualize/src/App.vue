e349e9dc1b008e0d13fcac1a892b69df1edd8f9d
lornd <lornd@qq.com>
2024-12-28 14:33:55 +0800

    feat:日志可视化，不同智能体分类展示

--- agent_network/visualize/src/App.vue
+++ agent_network/visualize/src/App.vue
@@ -1,14 +1,15 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import LogVisualize from './views/LogVisualize.vue';
 </script>
 
-<template>
+<template width="100%">
   <RouterView />
 </template>
 
 <style scoped>
-header {
+/* header {
   line-height: 1.5;
   max-height: 100vh;
 }
@@ -68,5 +69,5 @@ nav a:first-of-type {
     padding: 1rem 0;
     margin-top: 1rem;
   }
-}
+} */
 </style>
